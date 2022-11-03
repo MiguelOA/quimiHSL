a55233cf4554460ed38ee51e0aed3bee17ce5c16
Danmush <116917508+Danmush@users.noreply.github.com>
2022-11-03 15:15:26 -0700

    Update quimiHSL.py
    Cambio en el método de obtención de histograma RGB, removiendo el for

--- quimiHSL.py
+++ quimiHSL.py
@@ -21,11 +21,18 @@ def abrir():
 	myvar.grid(row=25, column=6)
 	image = cv2.imread(path)
 	
-	for i, col in enumerate(['b', 'g', 'r']):
-		hist = cv2.calcHist([image], [i], None, [256], [0, 256])
-		plt.plot(hist, color = col)
-		plt.xlim([0, 256])
-	plt.show() 
+	fig=plt.Figure(figsize=(5, 4), dpi=100)
+	ax=fig.add_subplot()
+	hist_r= cv2.calcHist([image], [2], None, [256], [0, 256])
+	hist_g= cv2.calcHist([image], [1], None, [256], [0, 256])
+	hist_b= cv2.calcHist([image], [0], None, [256], [0, 256])
+	xlim=list(range(0,256))
+	ax.plot(xlim, hist_r, "r",xlim, hist_g, "g",xlim, hist_b, "b")
+	ax.set_xlabel("Valor RGB")
+	ax.set_ylabel("Cuentas")
+	canvas = FigureCanvasTkAgg(fig, master=window)
+	canvas.draw()
+	canvas.get_tk_widget().grid(row=25, column=16)
 	
 	
 button1 = Button(window, text="abrir imagen", command=abrir).grid(row=0, column=0)
